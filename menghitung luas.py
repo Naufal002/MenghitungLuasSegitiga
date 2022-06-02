544fea56c1ddcbcc4e288b5bd0cc7ef61acd889e
Naufal002 <80965666+Naufal002@users.noreply.github.com>
2022-06-02 10:47:55 +0700

    first commit

---
+++ menghitung luas.py
@@ -0,0 +1,9 @@
+# Input Alas
+alas = float(input("Masukkan Alas: "))
+
+# Input Tinggi
+tinggi = float(input("Masukkan tinggi: "))
+
+# Count
+luas = (alas*tinggi)
+print('Luas Segitiga adalah %0.2f' %luas)
